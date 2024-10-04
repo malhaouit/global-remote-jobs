6a5d544f3a0750c297902977b812e21a1a7e4df1
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-04 22:23:35 +0100

    Display the fetched job data and add a default company logo

--- front-end/src/components/JobCard.jsx
+++ front-end/src/components/JobCard.jsx
@@ -1,21 +1,26 @@
 import React from "react";
 import './JobCard.css';
-import CompanyLogo from '../assets/web.jpg';
+import defaultCompanyLogo from '../assets/default-company-logo.png';
 
-const JobCard = ({ title, company, type, salary, location, posted }) => {
+const JobCard = ({ title, company, logo, location, salary_min, salary_max, posted_date }) => {
+  
   return (
     <div className="job-card">
       <h3>{title}</h3>
       <div className="company-info">
-        <img src={CompanyLogo} alt={`${company} logo`} className="company-logo" />
+        <img
+          src={logo} 
+          alt={`${company} logo`} 
+          className="company-logo" 
+          onError={(e) => e.target.src = defaultCompanyLogo}
+        />
         <p>{company}</p>
       </div>
       <div className="job-info">
         <span>{location}</span>
-        <span>{type}</span>
-        <span>{salary}</span>
+        <span>{salary_min ? `$${salary_min}` : ''} - {salary_max ? `$${salary_max}` : ''}</span>
       </div>
-      <p className="posted-time">{posted}</p>
+      <p className="posted-time">Posted on: {new Date(posted_date).toLocaleString()}</p>
     </div>
   );
 };
