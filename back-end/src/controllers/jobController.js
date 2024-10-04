e7d5b8f51d760ad14f2cd5616c7a40e5d630f343
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-04 19:42:09 +0100

    Add a new controller to handle filtering data

--- back-end/src/controllers/jobController.js
+++ back-end/src/controllers/jobController.js
@@ -10,4 +10,28 @@ const getJobs = async (req, res) => {
   }
 };
 
-module.exports = { getJobs };
+const getFilteredJobs = async (req, res) => {
+  try {
+    const response = await axios.get('https://remoteok.com/api');
+    const jobs = response.data;
+
+    // Filter jobs to only return the necessary fields
+    const filteredJobs = jobs
+    .filter(job => job.position && job.company)
+    .map(job => ({
+      title: job.position,
+      company: job.company,
+      logo: job.company_logo,
+      location: job.location,
+      salary_min: job.salary_min,
+      salary_max: job.salary_max,
+      posted_data: job.date,
+    }));
+
+    res.json(filteredJobs);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching filtered jobs' });
+  }
+};
+
+module.exports = { getJobs, getFilteredJobs };
