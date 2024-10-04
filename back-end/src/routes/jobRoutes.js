d78630d4279e3fc580be0a95b7dcae6444b17efc
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-04 19:41:42 +0100

    Add a new route to filter the fetched data

--- back-end/src/routes/jobRoutes.js
+++ back-end/src/routes/jobRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { getJobs }  = require('../controllers/jobController');
+const { getJobs, getFilteredJobs } = require('../controllers/jobController');  // Import both controllers
 
 const router = express.Router();
 
+// All jobs
 router.get('/', getJobs);
+
+// New route for filtered jobs
+router.get('/filtered', getFilteredJobs);
 
 module.exports = router;
