63805e53bdce1d86d9c284616b03289dab7cf3e5
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-07 00:41:02 +0100

    Add the job type fields to the job card and link the job cards to the job detail page

--- front-end/src/components/JobCard.jsx
+++ front-end/src/components/JobCard.jsx
@@ -1,27 +1,32 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import './JobCard.css';
 import defaultCompanyLogo from '../assets/default-company-logo.png';
 
-const JobCard = ({ title, company, logo, location, salary_min, salary_max, posted_date }) => {
-  
+const JobCard = ({ id, title, company, logo, location, job_type, salary, posted_date }) => {
   return (
-    <div className="job-card">
-      <h3>{title}</h3>
-      <div className="company-info">
-        <img
-          src={logo} 
-          alt={`${company} logo`} 
-          className="company-logo" 
-          onError={(e) => e.target.src = defaultCompanyLogo}
-        />
-        <p>{company}</p>
-      </div>
-      <div className="job-info">
-        <span>{location}</span>
-        <span>{salary_min ? `$${salary_min}` : ''} - {salary_max ? `$${salary_max}` : ''}</span>
-      </div>
-      <p className="posted-time">Posted on: {new Date(posted_date).toLocaleString()}</p>
-    </div>
+    <Link to={`/job/${id}`} className="job-card">
+        <h3>{title}</h3>
+        <div className="company-info">
+          <img
+            src={logo} 
+            alt={`${company} logo`} 
+            className="company-logo" 
+            onError={(e) => e.target.src = defaultCompanyLogo}
+          />
+          <p>{company}</p>
+        </div>
+        <div className="job-info">
+          <ul>
+            <li>{location}</li>
+            <li>{salary !== 'Not specified' ? salary : 'Salary not provided'}</li>
+            <li>{job_type}</li>
+          </ul>
+          {/* <span>{location}</span>
+          <span>{salary !== 'Not specified' ? salary : 'Salary not provided'}</span>
+          <span>{job_type}</span> */}
+        </div>
+        <p className="posted-time">Posted on: {new Date(posted_date).toLocaleDateString()}</p>
+    </Link>
   );
 };
 
