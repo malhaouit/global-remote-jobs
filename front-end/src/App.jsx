1e22af09fc2dc8b4cea8cd593644e5248f2e9969
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-07 00:33:42 +0100

    Create new route for job details page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import './App.css';
+import JobDetails from "./pages/JobDetails";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/job/:id' element={<JobDetails />} />
+      </Routes>
+    </Router>
   );
 };
 
