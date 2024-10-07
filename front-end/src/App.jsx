6881722a826f5f84481d64beb96a4c80dce6601e
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-07 19:28:17 +0100

    Add new route to JobListings page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./pages/HomePage";
 import './App.css';
 import JobDetails from "./pages/JobDetails";
+import JobListings from './pages/JobListings';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/job/:id' element={<JobDetails />} />
+        <Route path="/jobs" element={<JobListings />} />
       </Routes>
     </Router>
   );
