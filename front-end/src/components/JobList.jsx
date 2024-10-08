f03e2cb8a198e27bd0df534e9ad78b92edc362f4
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-08 21:14:38 +0100

    Add loading state

--- front-end/src/components/JobList.jsx
+++ front-end/src/components/JobList.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import JobCard from './JobCard';
 import './JobList.css';
 import { Link } from "react-router-dom";
 
 const JobList = ({ jobs }) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      setLoading(false);
+    } else {
+      setLoading(true);
+    }
+  }, [jobs]);
+
   const displayedJobs = jobs.slice(0, 12);
- 
+
   return (
     <section className="job-list">
       <h2>New Jobs</h2>
+
+      {/* Display a loading message or spinner while jobs are being fetched */}
+      {loading && (
+        <div className='loading-message'>
+          <p>Loading jobs, please wait...</p>
+        </div>
+      )}
+
+      {/* Display the no jobs message if no jobs are found */}
+      {!loading && jobs.length === 0 && (
+        <div className="no-jobs-message">
+          <p>No jobs available right now.</p>
+        </div>
+      )}
+
+      {/* Job Grid */}
       <div className="job-grid">
-        {jobs.length > 0 ? (
+        {!loading && jobs.length > 0 &&
           displayedJobs.map((job, index) => (
             <JobCard 
               key={index}
@@ -24,13 +50,15 @@ const JobList = ({ jobs }) => {
               posted_date={job.publication_date}
             />
           ))
-        ) : (
-          <p>No jobs available right now.</p>
-        )}
+        }
       </div>
-      <button className="more-jobs-btn">
-        <Link to="/jobs" className="no-style-link">More Jobs</Link>
-      </button>
+      
+      {/* More Jobs Button */}
+      {!loading && jobs.length > 0 && (
+        <button className="more-jobs-btn">
+          <Link to="/jobs" className="no-style-link">More Jobs</Link>
+        </button>
+      )}
     </section>
   );
 };
