e8a647fe60fae2d0aba06af6654f1dc81ec55f60
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-09 17:27:43 +0100

    Add new routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,10 @@ import HomePage from "./pages/HomePage";
 import './App.css';
 import JobDetails from "./pages/JobDetails";
 import JobListings from './pages/JobListings';
+import SignUpSeeker from './pages/SignUpSeeker';
+import Login from './pages/Login';
+import SignUpCompany from "./pages/SignUpCompany";
+import ChooseRole from './pages/ChooseRole';
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/job/:id' element={<JobDetails />} />
         <Route path="/jobs" element={<JobListings />} />
+        <Route path='/signup' element={<ChooseRole />} />
+        <Route path='/signup/seeker' element={<SignUpSeeker />} />
+        <Route path='/signup/company' element={<SignUpCompany />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
