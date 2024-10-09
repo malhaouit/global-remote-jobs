9cbdc408fefc33549f20fe7cb8a994324edae62a
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-09 01:29:20 +0100

    Add new route for login

--- back-end/src/routes/authRoutes.js
+++ back-end/src/routes/authRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { signUpSeeker, signUpCompany } = require('../controllers/authController');
+const { signUpSeeker, signUpCompany, login } = require('../controllers/authController');
 
 const router = express.Router();
 
+// Sign Up routes
 router.post('/signup/seeker', signUpSeeker);
 router.post('/signup/company', signUpCompany);
+
+// Login Route
+router.post('/login', login);
 
 module.exports = router;
