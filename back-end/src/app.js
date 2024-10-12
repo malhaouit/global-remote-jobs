09c14cd94e6e337ce0e3d3bfc4518b1746a5d717
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-12 22:18:03 +0100

    Serve /uploads directory

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const jobRoutes = require('./routes/jobRoutes');
 const authRoutes = require('./routes/authRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 const connectDB = require('./config/db');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
@@ -14,8 +17,18 @@ app.use(cors());
 // Middleware to parse JSON
 app.use(express.json());
 
+// Create the uploads directory if it doesn't exist
+// const uploadsDir = path.join(__dirname, 'uploads');
+
+// if (!fs.existsSync(uploadsDir)) {
+//   fs.mkdirSync(uploadsDir);
+//   console.log('Uploads directory created');
+// }
+
 // Routes
 app.use('/api/jobs', jobRoutes);
 app.use('/api', authRoutes);
+app.use('/uploads', express.static('uploads'));
+app.use('/api/profile', profileRoutes);
 
 module.exports = app;
