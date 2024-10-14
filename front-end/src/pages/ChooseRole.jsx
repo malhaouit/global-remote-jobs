f14219047432add70945c1d8e669c64ad81e8c9d
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-14 13:19:52 +0100

    Add two images and Go back to Home page

--- front-end/src/pages/ChooseRole.jsx
+++ front-end/src/pages/ChooseRole.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './ChooseRole.css';
 
+// Add images (replace the paths with your own images)
+import seekerIcon from '../assets/seeker-icon.svg'; 
+import companyIcon from '../assets/company-icon.svg'; 
+
 const ChooseRole = () => {
   return (
     <div className="choose-role-container">
@@ -10,18 +14,30 @@ const ChooseRole = () => {
         {/* Job Seeker Button */}
         <Link to="/signup/seeker" className="role-btn">
           <div className="role-box">
-            <h3>Job Seeker</h3>
-            <p>Find your dream remote job</p>
+            <div>
+              <h3>Job Seeker</h3>
+              <p>Find your dream remote job</p>
+            </div>
+            <img src={seekerIcon} alt="Job Seeker Icon" />
           </div>
         </Link>
 
         {/* Company Button */}
         <Link to="/signup/company" className="role-btn">
           <div className="role-box">
-            <h3>Company</h3>
-            <p>Find the right talent for your organization</p>
+            <div>
+              <h3>Company</h3>
+              <p>Find the right talent for your organization</p>
+            </div>
+            <img src={companyIcon} alt="Company Icon" />
           </div>
         </Link>
+      </div>
+
+      {/* New section for login and returning to home */}
+      <div className="additional-links">
+        <p>Already have an account? <Link to="/login">Login here</Link></p>
+        <p>Go back to <Link to="/">Home page</Link></p>
       </div>
     </div>
   );
