7e76447f8a8f75391f369e1796c7f28f2f948055
malhaouit <abdelmounaim.abdel@gmail.com>
2024-10-15 01:59:24 +0100

    Create GetInTouch component

---
+++ front-end/src/components/GetInTouch.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './GetInTouch.css';
+
+const GetInTouch = () => {
+  return (
+    <section className="git-section">
+      <div className="git-content">
+        <h2>You can reach to us any time</h2>
+        <button className="git-btn">Let's Connect</button>
+      </div>
+    </section>
+  );
+};
+
+export default GetInTouch;
